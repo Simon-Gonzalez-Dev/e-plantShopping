8adca38583a6f15638adf4e0f17585f3d70193b1
Your Name <user@example.com>
2025-04-04 20:42:26 -0400

    Configure project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
+  base: '/e-plantShopping/',
+  build: {
+    outDir: 'dist'
+  }
 })
